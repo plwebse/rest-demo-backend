22839c05503bb003ce8c85028d8f8ecf81aa86d3
Peter Lindblom <peter.lindblom.79@gmail.com>
2025-08-31 20:03:05 +0200

    More cors settings

--- src/main/java/se/plweb/rest_demo/RestDemoApplication.java
+++ src/main/java/se/plweb/rest_demo/RestDemoApplication.java
@@ -10,16 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class RestDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestDemoApplication.class, args);
+    }
 
     @Bean
     CommandLineRunner initDatabase(CustomerRepository repository) {
 
         return args -> {
             for (int i = 0; i < 300; i++) {
-                repository.add(new Customer("Peter Lindblom" + i, "Peter's address", "email"+i+"@plweb.se", "070" + i));
+                repository.add(new Customer("Peter Lindblom" + i, "Peter's address", "email" + i + "@plweb.se", "070" + i));
             }
         };
     }
@@ -29,7 +29,14 @@ public class RestDemoApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5173/");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173/")
+                        .allowedOrigins("http://localhost:8080/")
+                        .allowedOrigins("http://localhost:8081/")
+                        .allowedOrigins("http://127.0.0.1:5173/")
+                        .allowedOrigins("http://127.0.0.1:8080/")
+                        .allowedOrigins("http://127.0.0.1:8081/");
+
             }
         };
     }
